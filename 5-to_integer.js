941f49b8850130bfd311d85ad6ffa6a7bdeeaea3
Ishimwechille <012220538@isbatuniversity.com>
2025-09-09 17:35:26 +0300

    Add 5-to_integer.js script to convert argument to integer

---
+++ 5-to_integer.js
@@ -0,0 +1,8 @@
+const arg = process.argv[2]; // Get the first argument
+const number = parseInt(arg); // Convert to integer
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${number}`);
+}
