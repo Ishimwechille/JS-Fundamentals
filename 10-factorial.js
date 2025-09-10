551c3599e9ae05cc6f2b3174849df31e9a7b076b
Ishimwechille <012220538@isbatuniversity.com>
2025-09-10 09:52:35 +0300

    Add recursive factorial script (10-factorial.js)

---
+++ 10-factorial.js
@@ -0,0 +1,13 @@
+// Function to compute factorial recursively
+const factorial = (n) => {
+  if (isNaN(n) || n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+};
+
+// Get the argument from the command line
+const arg = parseInt(process.argv[2]);
+
+// Print the factorial
+console.log(factorial(arg));
