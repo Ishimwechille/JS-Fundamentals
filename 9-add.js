4e935beb25ea63c4b40a5817cda28f753c3dacbc
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-09-10 09:57:41 +0300

    Update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,8 +1,13 @@
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-
-function add(x, y) {
-  return x + y;
+// Recursive function to compute factorial
+function factorial(n) {
+  if (isNaN(n) || n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-console.log(add(a, b));
+// Get the argument from the command line
+const arg = parseInt(process.argv[2]);
+
+// Print the factorial
+console.log(factorial(arg));
